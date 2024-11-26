5ad7dfaae24c1aa458789762ad877485db42970c
Mohammed-issa0 <mohammed.issaopte@gmail.com>
2024-11-26 12:29:55 +0300

    pixel added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 import App from "./App.jsx";
+import ReactPixel from "react-facebook-pixel";
 import "./style.css";
-
 import AppRoutes from "./AppRoutes.jsx";
+
+// إعداد Facebook Pixel
+const pixelId = 468993109088850; // ضع هنا رقم الـ Pixel ID الخاص بك
+ReactPixel.init(pixelId); // تهيئة Facebook Pixel
+ReactPixel.pageView(); // تتبع أول زيارة للصفحة
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppRoutes />
